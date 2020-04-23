e58615716518ff2679b590eb0abe13c735e1d33e
Sai Darshan <saidarshan27@gmail.com>
2020-04-23 23:30:07 +0530

    add passport failure flash

--- app.js
+++ app.js
@@ -56,6 +56,19 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username or password' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
 passport.serializeUser(function(user, done) {
 	done(null, user);
   });
